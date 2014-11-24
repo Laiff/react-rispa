adbaab234058e1a58e270ac1601d5c8672b0ff19
Andrew Laiff <q.laiff@gmail.com>
2014-11-25 02:59:44 +0500

    First in server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by laiff on 25.11.14.
+ */
+
+console.log("Starting server...");
+
+// Init routes
+//require("./routes/auth");
+//require("./routes/api");
+//require("./routes/sockets");
+//require("./routes/pushState");
+//require("./routes/browserReload");
+
+console.log("Server started");
