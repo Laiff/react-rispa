88fdd737852554016d2332e08a2c4acb572fdc45
Andrew Laiff <q.laiff@gmail.com>
2014-11-26 00:13:48 +0500

    Explain folder structure

--- client/package.js
+++ client/package.js
@@ -2,4 +2,17 @@
  * Created by laiff on 25.11.14.
  *
  * TODO Explain client code structure
+ *
+ * directories:
+ * -|-client
+ *  +-- actions - contains actions for application,
+ *  |             files from this folder will be automatically registered as actions in RefluxApp
+ *  +-- components - contains jsx files for structural elements of application
+ *  +-- handlers - contains jsx (?) files witch used as handlers in react-router
+ *  +-- plugins - contains some useful parts of other components
+ *  +-- mixins - contains some useful mixin for components
+ *  +-- stores - contains store definitions for application?
+ *               files from this directory will be automatically registered as store in RefluxApp
+ *
+ *  Some other folders for organize code
  */
